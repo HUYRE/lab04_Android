0d29c38bb92db009cb8228d8f86729e1fe12078f
Anais <anais.flores@tecsup.edu.pe>
2024-09-08 12:54:01 -0500

    feat:add viewholacurso

--- Lab4_Anais/app/src/main/java/com/example/lab4_anais/MainActivity.kt
+++ Lab4_Anais/app/src/main/java/com/example/lab4_anais/MainActivity.kt
@@ -1,20 +1,22 @@
 package com.example.lab4_anais
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.* // Para Spacer y Grid
-import androidx.compose.foundation.lazy.LazyColumn // Para LazyColumn
-import androidx.compose.foundation.lazy.LazyRow // Para LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.* // Para Scaffold, FloatingActionButton, AlertDialog, etc.
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -22,195 +24,46 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApp()
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
-@Composable
-fun MyApp() {
-    Scaffold(
-        topBar = { TopAppBar(title = { Text("Lab 04 AnaisA") }) },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { /* Acción al hacer clic */ }) {
-                Text("+")
-            }
-        },
-        content = {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
-            ) {
-                LazyColumnExample()
-                Spacer(modifier = Modifier.height(16.dp))
-                LazyRowExample()
-                Spacer(modifier = Modifier.height(16.dp))
-                GridExample()
-                Spacer(modifier = Modifier.height(16.dp))
-                ShowAlertDialog()
+            BirthdayAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingCard()  // Llamamos a la función com.example.lab4_anais.GreetingCard
+                }
             }
         }
-    )
-}
-
-@Composable
-fun LazyColumnExample() {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.LightGray, RoundedCornerShape(8.dp))
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        items(10) { index ->
-            Text(
-                text = "Elemento $index",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier
-                    .background(Color.Blue, RoundedCornerShape(4.dp))
-                    .padding(8.dp)
-                    .clickable { /* Acción al hacer clic */ }
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
     }
 }
 
-@Composable
-fun LazyRowExample() {
-    LazyRow(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.LightGray, RoundedCornerShape(8.dp))
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        items(10) { index ->
-            Text(
-                text = "Item $index",
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White,
-                modifier = Modifier
-                    .background(Color.Green, RoundedCornerShape(4.dp))
-                    .padding(8.dp)
-                    .clickable { /* Acción al hacer clic */ }
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-        }
-    }
+class BirthdayAppTheme(function: @Composable () -> Unit) {
+
 }
 
 @Composable
-fun GridExample() {
+fun GreetingCard() {
     Column(
         modifier = Modifier
-            .padding(16.dp)
             .fillMaxWidth()
-            .background(Color.Yellow, RoundedCornerShape(8.dp))
-            .padding(8.dp)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        for (i in 0..1) { // Número de filas
-            Row(
-                horizontalArrangement = Arrangement.SpaceAround,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                for (j in 0..2) { // Número de columnas
-                    Box(
-                        modifier = Modifier
-                            .size(60.dp)
-                            .background(Color.Red, RoundedCornerShape(8.dp))
-                            .clickable { /* Acción al hacer clic */ },
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Item ${i * 3 + j}",
-                            color = Color.White,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                }
-            }
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-    }
-}
-
-@Composable
-fun ShowAlertDialog() {
-    var showDialog by remember { mutableStateOf(false) }
-
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Alerta") },
-            text = { Text("Este es un mensaje de alerta con funcionalidad.") },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        showDialog = false
-                        // Acción adicional al cerrar el diálogo
-                    }
-                ) {
-                    Text("Aceptar")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("Cancelar")
-                }
-            }
+        Text(
+            text = "¡Bienvenido al Curso!",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "¡Hola, Estudiante!",
+            fontSize = 20.sp
         )
     }
-
-    Button(
-        onClick = { showDialog = true },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        Text("Mostrar Alerta")
-    }
 }
+
+@Preview(showBackground = true)
 @Composable
-fun ShowAlertDialog() {
-    var showDialog by remember { mutableStateOf(false) }
-
-    if (showDialog) {
-        AlertDialog(
-            onDismissRequest = { showDialog = false },
-            title = { Text("Alerta") },
-            text = { Text("Este es un mensaje de alerta con funcionalidad.") },
-            confirmButton = {
-                Button(
-                    onClick = {
-                        showDialog = false
-                        // Acción adicional al cerrar el diálogo
-                    }
-                ) {
-                    Text("Aceptar")
-                }
-            },
-            dismissButton = {
-                Button(onClick = { showDialog = false }) {
-                    Text("Cancelar")
-                }
-            }
-        )
-    }
-
-    Button(
-        onClick = { showDialog = true },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
-    ) {
-        Text("Mostrar Alerta")
-    }
+fun PreviewGreetingCard() {
+    GreetingCard()
 }
-
